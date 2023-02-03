1cedfde45aa05be8508c6fe105a3f75519b9a49a
edwardPew <edik.tv@mail.ru>
2023-02-03 15:56:40 +0600

    added  post method to a server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,10 @@
 const Websocket=require('ws');
 const path=require('path');
-
 const wss= new Websocket.Server({port:8800});
-
 const express=require('express');
 const app=express();
 const server=require('http').createServer(app)
+//импорт библиотек
 
  
  
@@ -14,12 +13,27 @@ const server=require('http').createServer(app)
     wss.clients.forEach(eachClient=>{
             eachClient.send(JSON.stringify({msg:'hi'}))});
  })
+ //отсылает новости каждому подключённому клиенту 
+
+ app.use(express.urlencoded({
+    extended: true
+  }));
+  // исправляет ошибку "udefined"
  
- app.get('/',(req,res)=>{
+ app.get('/adminpost',(req,res)=>{
 
     res.sendFile(path.join(__dirname, '../server.html'));
 
  })
+//позволяет ввестии новости от администратора 
+
+ app.post('/adminpost',(req,res)=>{
+    console.log('postedDATA', req.body);
+    res.sendFile(path.join(__dirname, '../server.html'));
+ })
+ //обрабатывает новости администратора 
+
 
  server.listen(3000,()=>console.log('listeningOmportc3000'))
+ // запускает сервер на localhost:3000
  
