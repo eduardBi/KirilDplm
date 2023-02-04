f64ca4298924eacc8ea0b766b504034b257d7b2f
edwardPew <edik.tv@mail.ru>
2023-02-04 17:43:31 +0600

    added comment feature

--- server/server.js
+++ server/server.js
@@ -6,7 +6,17 @@ const app=express();
 const server=require('http').createServer(app)
 //импорт библиотек
 
- 
+
+ let posts=[
+   {
+      id:0,
+      title:'Kst found black hole'
+   },
+   {
+      id:1,
+      title:'Kst found black whore'
+   }
+ ]
  
 
  wss.on('connection',ws=>{
@@ -25,13 +35,30 @@ const server=require('http').createServer(app)
     res.sendFile(path.join(__dirname, '../server.html'));
 
  })
+
+ app.get('/posts/:id',(req,res)=>{
+
+   res.send(posts[req.params.id]);
+
+ })
+ //позволяет открыть каждый пост отдельно в новом файле 
+
+
 //позволяет ввестии новости от администратора 
 
  app.post('/adminpost',(req,res)=>{
     console.log('postedDATA', req.body);
     res.sendFile(path.join(__dirname, '../server.html'));
  })
+
  //обрабатывает новости администратора 
+
+ app.post('/postcomment/:id',(req,res)=>{
+   console.log('postedDATA', req.params.id);
+   console.log(req.body);
+   res.sendFile(path.join(__dirname, '../comment.html'));
+})
+//добавляет комментарий
 
 
  server.listen(3000,()=>console.log('listeningOmportc3000'))
