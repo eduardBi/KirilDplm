fb9bcfcbb8ec5dcf0c62c7270e1a575607e3c580
edwardPew <edik.tv@mail.ru>
2023-02-05 19:35:53 +0600

    mixing comment on the server side

--- server/server.js
+++ server/server.js
@@ -2,105 +2,91 @@ const Websocket=require('ws');
 const path=require('path');
 const wss= new Websocket.Server({port:8800});
 const express=require('express');
-const { copyFileSync } = require('fs');
+
 const app=express();
 const server=require('http').createServer(app)
-/*
-const { Server } = require("socket.io");
-const io = new Server(server);
-*/
+
+
 //импорт библиотек
 
 
  let posts=[
    {
       id:0,
-      title:'Kst found black hole'
+      title:'Kst found black hole',
+      comments:['my little comment','second'],
    },
    {
       id:1,
-      title:'Kst found black whore'
+      title:'Kst found black whore',
+      comments:[],
    }
  ]
  
+ 
+
+
+         let clients=[]
+
+
 
  wss.on('connection',ws=>{
-   
-         ws.onmessage = ({data}) => {
-            wss.clients.forEach(function each(client) {
-               if(JSON.parse(data).type=='openOnePost'){
-                  console.log('yes one')
-                  if (client === ws && client.readyState === Websocket.OPEN) {
-                     let idOfOpendPost=JSON.parse(data).id
-                     
-                    client.send(JSON.stringify(posts[idOfOpendPost]));
-                  }
-               }
-               if(JSON.parse(data).type=='openAllPosts'){
-                  console.log('openAllPosts')
-                  if (client === ws && client.readyState === Websocket.OPEN) {
-                    client.send(JSON.stringify( posts));
-                  }
-               }
-               if(JSON.parse(data).type=='leaveAcomment'){
-                  console.log('leaveAcomment')
-                  if (client === ws && client.readyState === Websocket.OPEN) {
-                    client.send(JSON.stringify( posts));
-                  }
-               }
-      });
 
-   }})
- //отсылает новости каждому подключённому клиенту 
+   ws.onmessage = ({data}) => {
+      wss.clients.forEach(function each(client) {
+         
+         if(JSON.parse(data).type=='seeIfCommentsUpdates' && JSON.parse(data).nummerOfcomments!=posts[JSON.parse(data).id].comments.length){
+            let difference=posts[JSON.parse(data).id].comments.length-JSON.parse(data).nummerOfcomments
+            let putchNewComment=[]
+            for (let i = 0; i < difference ; i++) {
+               
+               
+               let indexOfnewComment=posts[JSON.parse(data).id].comments.length-i-1;
+               putchNewComment.push(posts[JSON.parse(data).id].comments[indexOfnewComment])
+               
+            }
+            console.log(difference)
+            
+            client.send(JSON.stringify({type:'updateCommentSecton',comments:[...putchNewComment],id:JSON.parse(data).id}))
+            
 
- 
-//показывает каждую новость  
+         }else{
+            
+            console.log('leave')
+         }
+         if(JSON.parse(data).type=='openOnePost'){
+            
+            if (client === ws && client.readyState === Websocket.OPEN) {
+               let idOfOpendPost=JSON.parse(data).id
+               
+              client.send(JSON.stringify({...posts[idOfOpendPost],type:'showTitle'}));
+            }
+         }
+         if(JSON.parse(data).type=='openAllPosts'){
+            
+            if (client === ws && client.readyState === Websocket.OPEN) {
+              client.send(JSON.stringify( posts));
+            }
+         }
+         if(JSON.parse(data).type=='leaveAcomment'){
+            
+            
+               
+               
+               posts[JSON.parse(data).id].comments.push(JSON.parse(data).commentText)
+               console.log(JSON.parse(data).id)
+               
+            
+           
 
+         }
+      }
+         
+)};
 
- app.get('/',(req,res)=>{
-
-   res.sendFile(path.join(__dirname, '../fullList.html'));
-
- })
-
- app.use(express.urlencoded({
-    extended: true
-  }));
-  // исправляет ошибку "udefined"
-
-app.post('/postcomment/:id/comments',(req,res)=>{
-   console.log('postedDATA', req.body);
-   res.sendFile(path.join(__dirname, '../individualPost.html'));
-})
-//отправляет комментарии 
-
-
-
-
-  
- app.get('/adminpost',(req,res)=>{
-
-    res.sendFile(path.join(__dirname, '../server.html'));
-
- })
-//позволяет ввестии новости от администратора 
- 
- app.post('/adminpost',(req,res)=>{
-   console.log('postedDATA', req.body);
-   res.sendFile(path.join(__dirname, '../server.html'));
-})
-
-//обрабатывает новости администратора 
-
- 
-
- app.get('/posts/:id',(req,res)=>{
-
-   res.sendFile(path.join(__dirname, '../individualPost.html'));
-
- })
- //позволяет открыть каждый пост отдельно в новом файле 
-
+}
+         
+)
 
 
 
@@ -108,4 +94,4 @@ app.post('/postcomment/:id/comments',(req,res)=>{
 
  server.listen(3000,()=>console.log('listeningOmportc3000'))
  // запускает сервер на localhost:3000
- 
+
